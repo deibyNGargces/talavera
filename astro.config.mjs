335f2e97bc7d00ad5634edbc505c6a8647c1547c
Deiby Nino <deiby.ninog@uniagustiniana.edu.co>
2025-03-12 08:57:39 -0500

    ajuste incidencias Home y cambio imagenes home por formato webp

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), react()],
-  output: "hybrid",
+  output: "static",
   adapter: netlify(),
 });
